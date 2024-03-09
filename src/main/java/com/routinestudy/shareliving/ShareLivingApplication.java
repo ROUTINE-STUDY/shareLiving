c9cda460e1c96b9077448744740c8a63022d321f
sanghoomoon <sanghoo.moon@gmail.com>
2024-03-09 15:07:55 +0900

    프로젝트 세팅

---
+++ src/main/java/com/routinestudy/shareliving/ShareLivingApplication.java
@@ -0,0 +1,13 @@
+package com.routinestudy.shareliving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareLivingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareLivingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/routinestudy/shareliving/ShareLivingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.routinestudy.shareliving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareLivingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
