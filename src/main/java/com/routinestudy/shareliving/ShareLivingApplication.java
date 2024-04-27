8cda68955f53cfb776ac60f3109b4e9cb73c59a9
sanghoomoon <sanghoo.moon@gmail.com>
2024-04-27 19:26:20 +0900

    브랜치 테스트 커밋

--- src/main/java/com/routinestudy/shareliving/ShareLivingApplication.java
+++ src/main/java/com/routinestudy/shareliving/ShareLivingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing // JPA 활성화 ㅎㅎ
+@EnableJpaAuditing // JPA 활성화
 public class ShareLivingApplication {
 
     public static void main(String[] args) {
