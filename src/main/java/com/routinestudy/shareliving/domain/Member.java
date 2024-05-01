91c14323a83f713296acce20c7880d8a707d16c6
sanghoomoon <sanghoo.moon@gmail.com>
2024-05-01 13:17:44 +0900

    fix: #19 - 컬럼 사이즈 리뷰 관련 수정

--- src/main/java/com/routinestudy/shareliving/domain/Member.java
+++ src/main/java/com/routinestudy/shareliving/domain/Member.java
@@ -2,6 +2,7 @@ package com.routinestudy.shareliving.domain;
 
 import com.routinestudy.shareliving.common.BaseTimeEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,13 +21,19 @@ public class Member extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private int id;
+
+    @Size(max = 20)
     private String username;
+    @Size(max = 20)
     private String password;
+    @Size(max = 18)
     private String nickname;
 
     @CreatedDate
     private Timestamp pwChangeDate;
+    @Size(max = 20)
     private String insertId;
+    @Size(max = 20)
     private String modifyId;
 
 }
